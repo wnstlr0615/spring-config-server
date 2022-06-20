c4e42023ae55db9890ce13a9d804c4b255c00339
joonsik <ryan0@kakao.com>
2022-06-20 21:26:04 +0900

    Add Config-service user-service.yml 파일 추가

--- config-service/build.gradle.kts
+++ config-service/build.gradle.kts
@@ -23,6 +23,8 @@ dependencies {
     implementation("org.springframework.cloud:spring-cloud-config-server")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.cloud:spring-cloud-starter-bus-amqp")
+    implementation("org.springframework.cloud:spring-cloud-starter-bootstrap")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
